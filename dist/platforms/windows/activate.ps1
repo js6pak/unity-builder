4dd4ab0a7c84fe888442bc442ac949a5aeb8c6ca
js6pak <kubastaron@hotmail.com>
2023-02-18 02:30:59 +0100

    Use UnityBuildRunner on Windows

--- dist/platforms/windows/activate.ps1
+++ dist/platforms/windows/activate.ps1
@@ -1,7 +1,7 @@
 # Activates Unity
-& "C:\Program Files\Unity\Hub\Editor\$Env:UNITY_VERSION\Editor\Unity.exe" -batchmode -quit -nographics `
-                                                                          -username $Env:UNITY_EMAIL `
-                                                                          -password $Env:UNITY_PASSWORD `
-                                                                          -serial $Env:UNITY_SERIAL `
-                                                                          -projectPath "c:/BlankProject" `
-                                                                          -logfile | Out-Host
+& "$PSScriptRoot\UnityBuildRunner.exe" --unity-path "C:\Program Files\Unity\Hub\Editor\$Env:UNITY_VERSION\Editor\Unity.exe" `
+    -batchmode -quit -nographics `
+    -username $Env:UNITY_EMAIL `
+    -password $Env:UNITY_PASSWORD `
+    -serial $Env:UNITY_SERIAL `
+    -projectPath "c:/BlankProject"
--- dist/platforms/windows/entrypoint.ps1
+++ dist/platforms/windows/entrypoint.ps1
@@ -1,5 +1,7 @@
-# Activate Unity
-& "c:\steps\activate.ps1"
+$Env:UnityPath = "C:\Program Files\Unity\Hub\Editor\$Env:UNITY_VERSION\Editor\Unity.exe"
+
+Write-Host "entrypoint.ps1: Activate Unity"
+& "$PSScriptRoot\activate.ps1"
 
 # Import any necessary registry keys, ie: location of windows 10 sdk
 # No guarantee that there will be any necessary registry keys, ie: tvOS
@@ -8,8 +10,8 @@ Get-ChildItem -Path c:\regkeys -File | Foreach {reg import $_.fullname}
 # Register the Visual Studio installation so Unity can find it
 regsvr32 C:\ProgramData\Microsoft\VisualStudio\Setup\x64\Microsoft.VisualStudio.Setup.Configuration.Native.dll
 
-# Build the project
-& "c:\steps\build.ps1"
+Write-Host "entrypoint.ps1: Build the project"
+& "$PSScriptRoot\build.ps1"
 
-# Free the seat for the activated license
-& "c:\steps\return_license.ps1"
+Write-Host "entrypoint.ps1: Free the seat for the activated license"
+& "$PSScriptRoot\return_license.ps1"
